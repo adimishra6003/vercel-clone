147056c74b7f76f6cfb8a0e47769965d11e77cc0
adimishra6003 <adityamishra120799@gmail.com>
2024-04-25 19:36:26 +0530

    created generate function for session id generation

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,10 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.listen(3000);
-app.get('/test', (req, res) => {
-    res.status(200).json({ msg: "Test" });
+app.post('/deploy', (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    console.log(repoUrl);
+    res.json({});
 });
